f0e80cc386b3b9cb7ab42e8e5df04fa727b421a2
Scott <Scottt@ScottLaptop>
2014-11-21 01:55:20 -0500

    Added in building of cache. Each song is one line artist title time format. Now checks for connection and works offline rather than just crashing. Bug - If listening to 2+ songs it wont cache them.

--- Last.fmPlayer/src/Testing/Scrobbler.java
+++ Last.fmPlayer/src/Testing/Scrobbler.java
@@ -1,12 +1,13 @@
 package Testing;
 
 
+import java.io.FileWriter;
 import java.util.ArrayList;
 
+import de.umass.lastfm.Artist;
 import de.umass.lastfm.Authenticator;
 import de.umass.lastfm.Session;
 import de.umass.lastfm.Track;
-import de.umass.lastfm.Artist;
 
 public class Scrobbler 
 {//Scrobble class
@@ -15,7 +16,13 @@ public class Scrobbler
     static String user = "TestAccount3461"; 
     static String password = "meowmix"; 
     		//Create session to communicate with last.fm server
-    static Session session = Authenticator.getMobileSession(user, password, key, secret);
+    static Session session;
+    private String cacheFile = "cache.txt";
+    
+    //java.lang.ExceptionInInitializerError 
+    //de.umass.lastfm.CallException
+    //java.net.UnknownHostException
+    
     static ArrayList<MyMediaFrame> songCache = new ArrayList();
     
     private String url = new String();
@@ -25,6 +32,8 @@ public class Scrobbler
     private String title = new String();
     private String artist = new String();
     private String album = new String();
+    
+    private int offlineFlag = 0;
     
     public void main(String args[])
     {//Main
@@ -37,15 +46,31 @@ public class Scrobbler
     	title = null;
     	artist = null;
     	album = null;
+    	 try
+    	 {
+    	    	session = Authenticator.getMobileSession(user, password, key, secret);
+    	 }
+    	 catch(Exception e)
+    	 {
+    		 offlineFlag = 1;
+    	 }
     }//Default constructor
     
     public Scrobbler(MyMediaFrame track)
     {//Constructor with track
+    	try{
+	    	session = Authenticator.getMobileSession(user, password, key, secret);
+    	}
+		catch(Exception e)
+		{
+			offlineFlag = 1;
+		}
     	//Get metadata from MyMediaFrame class
     	title = track.getTitle();
     	artist = track.getArtist();
     	album = track.getAlbum();
     	getArtistInfo(artist);
+    	
     }//Constructor with track
     
     												/**METHODS**/
@@ -53,18 +78,33 @@ public class Scrobbler
     					//Scrobble currently playing track
 	public void scrobbleCurrent(MyMediaFrame track)
 	{//scrobbleCurrent
-			//Get current time
-		
+		//Get current time
 		int now = (int) (System.currentTimeMillis() / 1000);
+		
+		if(offlineFlag == 1)
+		{//Offline
+				//Add to cache
+			addToCache(track, now);
+			return;
+		}//Offline
 
 		Track.scrobble(artist, title, now, session);
 		getArtistInfo(artist);
 		return;
 	}//scrobbleCurrent
 	
-	public void buildCache()
+	public void addToCache(MyMediaFrame track, int timeStamp)
 	{//Build cache
 		
+		try
+		{
+	        FileWriter fw = new FileWriter(cacheFile, true);
+	        fw.write(artist+" "+title+" "+timeStamp+"\n");
+	        fw.close();
+		}
+		catch(Exception e)
+		{
+		}
 		return;
 	}//Build cache
 	
@@ -75,7 +115,7 @@ public class Scrobbler
 	
 	public void setNowPlaying(MyMediaFrame track)
 	{//Set now playing
-		Track.updateNowPlaying(artist, title, session);
+//		Track.updateNowPlaying(artist, title, session);
 	}//Set now playing
 	
 	
