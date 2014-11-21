fac2cca6048ce587dbff8cc8f12077200ba0cee6
Scott <Scottt@ScottLaptop>
2014-11-21 03:02:05 -0500

    Quickly moved Kevin's UI over to work with the rest of the program. Now I sleep.

--- Last.fmPlayer/src/Testing/MyMediaFrame.java
+++ Last.fmPlayer/src/Testing/MyMediaFrame.java
@@ -1,6 +1,14 @@
 package Testing;
 
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Container;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 import java.awt.GridLayout;
+import java.awt.Insets;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
@@ -18,6 +26,9 @@ import javax.swing.JButton;
 import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 import javax.swing.JPanel;
 import javax.swing.JSlider;
 import javax.swing.SwingConstants;
@@ -51,28 +62,105 @@ public class MyMediaFrame extends JFrame implements ActionListener, ChangeListen
 	private JLabel artistLabel = new JLabel();
 	private JLabel albumLabel = new JLabel();
 	private JLabel lengthLabel = new JLabel();
+	private JMenuItem AddToPlaylist, exitMI, ViewPlayer, ViewDescription, OptionSettings, HelpAbout;
+	private JButton playerButton, descriptionButton, settingsButton;
+	private static final int PREF_MIN_WIDTH = 600;
+	private static final int PREF_MIN_HEIGHT = 600;
 	
 	Scrobbler scrobbler = new Scrobbler();
 	
 	public MyMediaFrame(File playFile)
 	{
+		
 		JFXPanel fxPanel = new JFXPanel();
 		Media track = new Media(new File("stuff.mp3").toURI().toString());
 		mediaPlayer = new MediaPlayer(track);
-
+		this.setMinimumSize(new Dimension(PREF_MIN_WIDTH, PREF_MIN_HEIGHT));
+		this.setVisible(true);
+		Container Pane = this.getContentPane();
+		Pane.setLayout(new BorderLayout());
+		
+		//The Menu bar
+		JMenuBar mb = new JMenuBar();
+		this.setJMenuBar(mb);
+		//Menu Items
+		JMenu fileMenu = new JMenu("File");
+		JMenu ViewMenu = new JMenu("View");
+		JMenu OptionMenu = new JMenu("Options");
+		JMenu HelpMenu = new JMenu("Help");
+		//Add Menu to Menu Bar
+		mb.add(fileMenu);
+		mb.add(ViewMenu);
+		mb.add(OptionMenu);
+		mb.add(HelpMenu);
+		        
+		//Add Items to the FileMenu
+		AddToPlaylist = new JMenuItem("AddToPlaylist");
+		exitMI = new JMenuItem("Exit");
+		fileMenu.add(AddToPlaylist);
+		fileMenu.add(exitMI);
+		AddToPlaylist.addActionListener(this);
+		exitMI.addActionListener(this);
+		        
+		//Add Items to the ViewMenu
+		ViewPlayer = new JMenuItem("Music Player");
+		ViewDescription = new JMenuItem("Track Description");
+		ViewMenu.add(ViewPlayer);
+		ViewMenu.add(ViewDescription);
+		ViewPlayer.addActionListener(this);
+		ViewDescription.addActionListener(this);
+		        
+		//Add Items to OptionMenu
+		OptionSettings = new JMenuItem("Settings");
+		OptionMenu.add(OptionSettings);
+		OptionSettings.addActionListener(this);
+		        
+		//Add Items to HelpMenu
+		HelpAbout = new JMenuItem("About");
+		HelpMenu.add(HelpAbout);
+		HelpAbout.addActionListener(this);
+		        
+		//Buttons for the sidebar
+		JPanel Sidebar = new JPanel();
+		Sidebar.setBackground(Color.GRAY);
+		Sidebar.setLayout(new GridBagLayout());
+		        
+		GridBagConstraints c = new GridBagConstraints();
+		c.fill = GridBagConstraints.HORIZONTAL;
+		c.insets = new Insets(3,3,3,3);
+		c.weightx = 0.5;
+		c.gridx = 0;
+		        
+		    	
+		playerButton = new JButton("Player");
+		descriptionButton = new JButton("Description");
+		settingsButton = new JButton("Settings");
+		playerButton.addActionListener(this);
+		descriptionButton.addActionListener(this);
+		settingsButton.addActionListener(this);
+		        
+		Sidebar.add(playerButton, c);
+		c.gridy = 1;
+		Sidebar.add(descriptionButton, c);
+		c.gridy = 2;
+		Sidebar.add(settingsButton, c);
+		        
+		 
+		JPanel buttonPanel = new JPanel();
+		buttonPanel.setBackground(Color.DARK_GRAY);
+		buttonPanel.setLayout(new FlowLayout(FlowLayout.CENTER));
+		
 		play = new JButton("Play");
 		stop = new JButton("Stop");
 		open = new JButton("Open");
 		exit = new JButton("Exit");
+		
 		volumeSlider = new JSlider(SwingConstants.HORIZONTAL, 0, 100, 100);
 		volumeSlider.setMinorTickSpacing(5);
 		volumeSlider.setMajorTickSpacing(25);
 		volumeSlider.setPaintTicks(true);
 		volumeSlider.setPaintLabels(true);
 
-			
-		JPanel buttonPanel = new JPanel();
-		buttonPanel.setBorder(new TitledBorder(new EtchedBorder(), "Command"));
 		buttonPanel.add(play);
 		buttonPanel.add(stop);
 		buttonPanel.add(open);
@@ -84,9 +172,8 @@ public class MyMediaFrame extends JFrame implements ActionListener, ChangeListen
 		exit.addActionListener(this); 
 		volumeSlider.addChangeListener(this);
 
-		JPanel volumePanel = new JPanel();
-		volumePanel.setBorder(new TitledBorder(new EtchedBorder(), "Volume"));
-		volumePanel.add(volumeSlider);
+		
+		buttonPanel.add(volumeSlider);
 		
 		JPanel currentlyPlaying = new JPanel();
 		currentlyPlaying.setBorder(new TitledBorder(new EtchedBorder(), "Currently Playing"));
@@ -104,14 +191,17 @@ public class MyMediaFrame extends JFrame implements ActionListener, ChangeListen
 		infoPanel.add(artistPlaycount);
 		
 		JPanel panel = new JPanel();
-		panel.setLayout(new GridLayout(2, 1, 25, 25));
+	/*	panel.setLayout(new GridLayout(2, 1, 25, 25));
 		panel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
 		panel.add(buttonPanel);
 		panel.add(volumePanel);	
 		panel.add(currentlyPlaying);
-		panel.add(infoPanel);
+		panel.add(infoPanel);*/
 
-		this.setContentPane(panel);
+		Pane.add(Sidebar, BorderLayout.WEST);
+        Pane.add(buttonPanel, BorderLayout.SOUTH);
+        this.pack();
+	//	this.setContentPane(panel);
 		
 		checkCache();
 	}
@@ -247,4 +337,3 @@ public class MyMediaFrame extends JFrame implements ActionListener, ChangeListen
 		return Album;
 	}
 }
-
