e54d00bffd92dce7025ea165343b7ccdfb9774d6
pilajore <cse13192@cse.yorku.ca>
2015-02-20 13:23:26 -0500

    Restoring class that is needed to run the application. Removing it was a mistake. Oh, the things that happen when you look at a project you haven't worked on in months.

---
+++ Last.fmPlayer/src/mainApp/MyMedia.java
@@ -0,0 +1,46 @@
+package mainApp;
+
+import java.io.File;
+import java.io.FileInputStream;
+
+import javax.swing.*;
+import javax.swing.border.*;
+import javax.swing.event.*;
+import javax.sound.sampled.FloatControl;
+
+import java.awt.*;
+import java.awt.event.*;
+
+import javafx.embed.swing.JFXPanel;
+import javafx.scene.media.*;
+
+public class MyMedia 
+{
+
+	public static void main(String[] args) throws Exception
+	{
+		// TODO Auto-generated method stub
+		
+	      try {
+	          UIManager.setLookAndFeel(
+	             UIManager.getSystemLookAndFeelClassName());
+	       } catch (Exception e) {}
+	    
+	    //File soundFile = new File("stuff.mp3");
+	    MyMediaFrame frame = new MyMediaFrame();
+	    frame.setTitle("ScrobbLord!");
+	    //frame.setTitle("DemoSoundVolume");
+	 	frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+	    frame.pack();
+	    frame.setVisible(true);
+	       
+			/*JFXPanel fxPanel = new JFXPanel();
+		    Media track = new Media(new File("stuff.mp3").toURI().toString());
+		    MediaPlayer mediaPlayer = new MediaPlayer(track);
+		    mediaPlayer.play();
+		    mediaPlayer.stop();
+		    mediaPlayer.setVolume(0.1);
+		    System.out.println(mediaPlayer.volumeProperty());*/
+		    
+	}
+}
