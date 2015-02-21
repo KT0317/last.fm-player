63ef0994660f0007778754e90645f4d95b4ffc29
pilajore <cse13192@cse.yorku.ca>
2015-02-21 18:03:46 -0500

    A full test suite is now included. Forgot to mention in last commit: now a test account.txt file is added with credentials to a playground Lat.fm accont created particularly for the testing suite.

---
+++ Last.fmPlayer/TestCases/mainApp/AllTests.java
@@ -0,0 +1,11 @@
+package mainApp;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ FileCompatibilityTest.class, FullTagTest.class, TitleTest.class })
+public class AllTests {
+
+}
